6316a0532df5bd0f9a4c9283d01144fb093b02fd
Pavlo Malysh <mal.pawlo@gmail.com>
2025-10-04 11:19:47 +0300

    update

--- src/server.js
+++ src/server.js
@@ -26,6 +26,7 @@ export default function setupServer() {
     app.get("/contacts", async (req, res) => {
         const contacts = await getAllContacts();
         res.status(200).json({
+            status: 200,
             message: "Successfully found contacts!",
             data: contacts
         });
@@ -43,6 +44,7 @@ export default function setupServer() {
         };
 
         res.status(200).json({
+            status: 200,
             message: `Successfully found contact with id ${contactId}!`,
             data: contact,
         });
