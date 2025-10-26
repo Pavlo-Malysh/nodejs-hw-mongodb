3bcd66ee236bb6be6fc6e22629dcbb3a8fc14e96
Pavlo Malysh <mal.pawlo@gmail.com>
2025-10-26 22:44:08 +0200

    add

--- src/server.js
+++ src/server.js
@@ -1,22 +1,28 @@
-
+import * as fs from "node:fs";
 import express from "express";
 import cors from "cors";
 import pino from "pino-http";
+import swaggerUI from "swagger-ui-express";
 import { errorHandler } from "./middlewares/errorHandler.js";
 import { notFoundHandler } from "./middlewares/notFoundHandler.js";
 import routers from "./routers/index.js";
 import cookieParser from "cookie-parser";
 import { PORT, UPLOAD_DIR } from "./constants/index.js";
+import path from "node:path";
 
 
 
 export default function setupServer() {
+    const SWAGGER_DOCUMENT = JSON.parse(fs.readFileSync(path.join("docs", "swagger.json")));
 
     const app = express();
 
     app.use(express.json());
     app.use(cors());
     app.use(cookieParser());
+
+
+    app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(SWAGGER_DOCUMENT));
 
     app.use(
         pino({
